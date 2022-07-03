4130b0ed3ca34e1aee7f3d3f98f0a2bfd6b5ad0b
Serhii Shramko <shramko.web@yahoo.com>
2022-07-03 22:37:59 +0200

    style: linter fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   // eslint-disable-next-line react/jsx-props-no-spreading
