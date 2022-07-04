dc80d67edbbe91b579f0b5f843d6c53a2a520adb
Serhii Shramko <shramko.web@yahoo.com>
2022-07-04 12:44:37 +0200

    refactor: rm unused code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,5 @@
 import type { AppProps } from 'next/app';
 
-import '../styles/globals.css';
-
 function MyApp({ Component, pageProps }: AppProps) {
   // eslint-disable-next-line react/jsx-props-no-spreading
   return <Component {...pageProps} />;
