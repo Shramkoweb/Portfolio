1d35abd7d9d0190f91c33c07e55a084af5001082
Serhii Shramko <shramko.web@yahoo.com>
2022-07-08 22:44:40 +0200

    build(deps): add tailwindcss/typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
+const { spacing } = require("tailwindcss/defaultTheme");
+
 /** @type {import("tailwindcss").Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-  ], theme: {
+  ],
+  theme: {
     extend: {
       colors: {
         gray: {
@@ -19,7 +22,76 @@ module.exports = {
           900: "#111111",
         },
       },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: theme("colors.gray.700"),
+            a: {
+              color: theme("colors.blue.500"),
+              "&:hover": {
+                color: theme("colors.blue.700"),
+              },
+              code: { color: theme("colors.blue.400") },
+            },
+            "h2,h3,h4": {
+              "scroll-margin-top": spacing[32],
+            },
+            thead: {
+              borderBottomColor: theme("colors.gray.200"),
+            },
+            code: { color: theme("colors.pink.500") },
+            "blockquote p:first-of-type::before": false,
+            "blockquote p:last-of-type::after": false,
+          },
+        },
+        dark: {
+          css: {
+            color: theme("colors.gray.200"),
+            a: {
+              color: theme("colors.blue.400"),
+              "&:hover": {
+                color: theme("colors.blue.600"),
+              },
+              code: { color: theme("colors.blue.400") },
+            },
+            blockquote: {
+              borderLeftColor: theme("colors.gray.700"),
+              color: theme("colors.gray.300"),
+            },
+            "h2,h3,h4": {
+              color: theme("colors.gray.100"),
+              "scroll-margin-top": spacing[32],
+            },
+            hr: { borderColor: theme("colors.gray.700") },
+            ol: {
+              li: {
+                "&:before": { color: theme("colors.gray.500") },
+              },
+            },
+            ul: {
+              li: {
+                "&:before": { backgroundColor: theme("colors.gray.500") },
+              },
+            },
+            strong: { color: theme("colors.gray.100") },
+            thead: {
+              th: {
+                color: theme("colors.gray.100"),
+              },
+              borderBottomColor: theme("colors.gray.600"),
+            },
+            tbody: {
+              tr: {
+                borderBottomColor: theme("colors.gray.700"),
+              },
+            },
+          },
+        },
+      }),
     },
   },
-  plugins: [],
+  variants: {
+    typography: ["dark"],
+  },
+  plugins: [require("@tailwindcss/typography")],
 };
