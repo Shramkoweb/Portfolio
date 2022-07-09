e9f56ae780c420e9c07c3c9234164c7bef4fdc4c
Serhii Shramko <shramko.web@yahoo.com>
2022-07-09 18:06:42 +0200

    refactor: change giv to mp4

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,10 +1,7 @@
 import Link from 'next/link';
 import Head from 'next/head';
-import Image from 'next/future/image';
 import * as Sentry from '@sentry/nextjs';
 import { useEffect } from 'react';
-
-import notFound from '../public/static/images/404.gif';
 
 function NotFound() {
   useEffect(() => {
@@ -20,7 +17,9 @@ function NotFound() {
         <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-8 text-black dark:text-white">
           YOU&apos;RE IN THE WRONG PLACE
         </h1>
-        <Image className="mb-8 w-full" src={notFound} alt="" />
+        <video loop autoPlay muted className="mb-8 w-full">
+          <source src="/static/images/404.mp4" type="video/mp4" />
+        </video>
         <Link href="/">
           <a
             className="p-1 sm:p-4 w-64 font-bold mx-auto bg-gray-200 dark:bg-gray-800 text-center rounded-md text-black dark:text-white"
