ec7afeca4e96de1c31ac3d9abc7b32968b36e3b9
Serhii Shramko <shramko.web@yahoo.com>
2022-07-09 22:20:23 +0200

    feat: add GA (#155)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,21 @@
 import type { AppProps } from 'next/app';
 
 import { Layout } from '@/components/layout';
+import { GoogleAnalytics } from '@/components/google-analytics';
 
 import 'styles/globals.css';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      {isProduction && <GoogleAnalytics />}
+      <Layout>
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
