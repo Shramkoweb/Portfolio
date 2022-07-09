5f4cf3f8c35b3e42c0b45706eaf20ae9a75f4145
Serhii Shramko <shramko.web@yahoo.com>
2022-07-09 18:35:34 +0200

    feat: add blog page

--- pages/about.tsx
+++ pages/about.tsx
@@ -15,7 +15,7 @@ function AboutPage() {
       </Head>
       <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-8 w-full">
         <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
-          About Me
+          <u>About Me</u>
         </h1>
 
         <div className="w-full mb-8 flex">
