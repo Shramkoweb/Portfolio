7ba2c98c02df24ec82c846a3f143fe900e8b8a9a
Serhii Shramko <shramko.web@yahoo.com>
2022-07-20 11:55:30 +0200

    refactor: different next config by ENV

--- next.config.js
+++ next.config.js
@@ -38,7 +38,7 @@ const securityHeaders = [
 ];
 
 /** @type {import("next").NextConfig} */
-const moduleExports = {
+const nextConfig = {
   swcMinify: true,
   experimental: {
     images: {
@@ -59,8 +59,11 @@ const moduleExports = {
   },
 };
 
-const sentryWebpackPluginOptions = {
-  silent: true,
-};
+const nextConfigByEnv = {
+  production: withSentryConfig(nextConfig, {
+    silent: true
+  }),
+  development: nextConfig
+}
 
-module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
+module.exports = nextConfigByEnv[process.env.NODE_ENV];
