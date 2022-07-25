d3796d62df7a984c6eeaa38937802c731c1dc2c4
Serhii Shramko <shramko.web@yahoo.com>
2022-07-25 10:45:34 +0200

    style: rm unused code

--- components/blog-post-preview/blog-post-preview.tsx
+++ components/blog-post-preview/blog-post-preview.tsx
@@ -11,13 +11,9 @@ export function BlogPostPreview({ slug, title, excerpt }: {
       <a className="w-full">
         <div className="w-full mb-8">
           <div className="flex flex-col justify-between md:flex-row">
-            <h4 className="w-full mb-2 text-lg font-medium text-gray-900 md:text-xl dark:text-gray-100">
+            <h3 className="w-full mb-2 text-lg font-medium text-gray-900 md:text-xl dark:text-gray-100">
               {title}
-            </h4>
-            {/* TODO implement views counter */}
-            {/* <p className="w-32 mb-4 text-left text-gray-500 md:text-right md:mb-0"> */}
-            {/*  {`${views ? Number(views).toLocaleString() : '–––'} views`} */}
-            {/* </p> */}
+            </h3>
           </div>
           <p className="text-gray-600 dark:text-gray-400">{excerpt}</p>
         </div>
