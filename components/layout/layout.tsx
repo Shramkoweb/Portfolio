61e7cfd405d89e51f94c2f0984d1e5787c94c204
Serhii Shramko <shramko.web@yahoo.com>
2022-08-05 17:02:37 +0200

    fix(ally): rm not recommend attributes
    https://web.dev/responsive-web-design-basics/#accessible-viewport

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -19,7 +19,7 @@ export function Layout({ children }: PropsWithChildren) {
         <title>{TITLE}</title>
         <meta
           name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
+          content="width=device-width, user-scalable=no, initial-scale=1.0"
         />
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
         <meta name="robots" content="follow, index" />
