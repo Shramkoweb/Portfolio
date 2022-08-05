2054d1cbfc483060d837774689e1923654b207a7
Serhii Shramko <shramko.web@yahoo.com>
2022-08-05 17:35:30 +0200

    feat: correct theme-color to different color-scheme

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -33,8 +33,8 @@ export default function Document() {
           href="/static/favicons/safari-pinned-tab.svg"
           rel="mask-icon"
         />
-        <meta content="#ffffff" name="theme-color" />
-        <meta content="#ffffff" name="msapplication-TileColor" />
+        <meta content="#f9fafb" name="theme-color" media="(prefers-color-scheme: light)" />
+        <meta content="#111111" name="theme-color" media="(prefers-color-scheme: dark)" />
         <meta
           content="/static/favicons/browserconfig.xml"
           name="msapplication-config"
