a4ab35b5882a62dcbc8280846fe0ed1bda59f519
Serhii Shramko <shramko.web@yahoo.com>
2022-08-05 17:35:06 +0200

    fix(ally): noopener to _blank links

--- components/footer-link/footer-link.tsx
+++ components/footer-link/footer-link.tsx
@@ -9,7 +9,7 @@ export function FooterLink({ href, children }: PropsWithChildren<FooterLinkProps
     <a
       className="text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white transition-colors"
       target="_blank"
-      rel="noreferrer"
+      rel="noopener noreferrer"
       href={href}
     >
       {children}
