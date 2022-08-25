92b6cdbc3b98952de4dc08e4d6541ebb6e002f5c
Serhii Shramko <shramko.web@yahoo.com>
2022-08-25 15:35:24 +0200

    chore(sec): add STS headers

--- next.config.js
+++ next.config.js
@@ -34,7 +34,11 @@ const securityHeaders = [
   {
     key: 'X-XSS-Protection',
     value: '1; mode=block'
-  }
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains; preload'
+  },
 ];
 
 /** @type {import("next").NextConfig} */
