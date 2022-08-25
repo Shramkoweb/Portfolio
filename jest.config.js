fe9c0f2150ebb0f4526172098dc424a49a110c08
Serhii Shramko <shramko.web@yahoo.com>
2022-08-25 15:35:40 +0200

    style: fix prettier

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,10 @@ const createJestConfig = nextJest({
 
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@/components/(.*)$': '<rootDir>/components/$1',
+    '^@/pages/(.*)$': '<rootDir>/pages/$1',
+  },
   testEnvironment: 'jest-environment-jsdom'
 };
 
