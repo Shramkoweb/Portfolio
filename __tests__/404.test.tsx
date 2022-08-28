3772e0176a012b3e2ff2e4f353f535900a1f7ba1
Serhii Shramko <shramko.web@yahoo.com>
2022-08-28 21:02:31 +0200

    test: 404 page rm beforeAll render page

--- __tests__/404.test.tsx
+++ __tests__/404.test.tsx
@@ -5,24 +5,26 @@ import NotFoundPage from '@/pages/404';
 
 jest.mock('@sentry/nextjs');
 
-beforeAll(() => {
-  render(<NotFoundPage />);
-});
-
 describe('404 page', () => {
   test('renders with correct heading', () => {
+    render(<NotFoundPage />);
+
     const heading = screen.getByRole('heading', {
-      name: 'YOU\'RE IN THE WRONG PLACE',
+      name: "YOU'RE IN THE WRONG PLACE",
     });
 
     expect(heading).toBeInTheDocument();
   });
 
   test('send sentry error on render', () => {
+    render(<NotFoundPage />);
+
     expect(Sentry.captureException).toHaveBeenCalledWith(new Error('404'));
   });
 
   test('send sentry error only on initial render', () => {
+    render(<NotFoundPage />);
+
     expect(Sentry.captureException).toBeCalledTimes(1);
   });
 });
