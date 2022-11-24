7fda71860c8952c40183eb9a23374444c5539ed8
Serhii Shramko <shramko.web@yahoo.com>
2022-11-24 13:50:00 +0100

    feat: made DashboardCard full interactive

--- components/dashboard-card/blog-views.tsx
+++ components/dashboard-card/blog-views.tsx
@@ -2,15 +2,19 @@ import useSWR from 'swr';
 
 import { Views } from 'lib/types';
 import { fetcher } from '@/lib/fetcher';
+
 import { DashboardCard } from '@/components/dashboard-card/dashboard-card';
 
 export function BlogViewsCard() {
   const { data } = useSWR<Views>('/api/views', fetcher);
 
   const blogViews = data?.total;
-  const link = 'https://shramko.dev/blog';
 
   return (
-    <DashboardCard header="Total blog views" link={link} metric={blogViews} />
+    <DashboardCard
+      header="Total blog views"
+      link="https://shramko.dev/blog"
+      metric={blogViews}
+    />
   );
 }
--- components/dashboard-card/github-followers.tsx
+++ components/dashboard-card/github-followers.tsx
@@ -2,6 +2,7 @@ import useSWR from 'swr';
 
 import { fetcher } from 'lib/fetcher';
 import { GitHub } from 'lib/types';
+
 import { DashboardCard } from '@/components/dashboard-card/dashboard-card';
 
 export function GithubFollowers() {
@@ -9,9 +10,11 @@ export function GithubFollowers() {
 
   const followers = data?.followers;
 
-  const link = 'https://github.com/Shramkoweb';
-
   return (
-    <DashboardCard header="GitHub Followers" link={link} metric={followers} />
+    <DashboardCard
+      header="GitHub Followers"
+      link="https://github.com/Shramkoweb"
+      metric={followers}
+    />
   );
 }
