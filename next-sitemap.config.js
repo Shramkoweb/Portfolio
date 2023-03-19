3b55ac5c311b798a7f12a9c7566c95b68d94e15d
Serhii Shramko <shramko.web@yahoo.com>
2023-03-19 14:11:15 -0400

    fix: correct seo title on snippet pages (#277)
    * fix: correct seo title on snippet pages
    
    * feat: update sitemap.xml

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,29 @@
-/** @type {import("next-sitemap").IConfig} */
+const pathLevelToPriority = {
+  0: 1,
+  1: 0.9,
+  2: 0.8,
+  3: 0.7
+};
 
+const getPathDepthLevel = (path) => {
+  if (path === '/') {
+    return 0;
+  }
+
+  return path.split('/').length - 1;
+};
+
+/** @type {import("next-sitemap").IConfig} */
 module.exports = {
   siteUrl: 'https://shramko.dev',
   generateIndexSitemap: false,
-  changefreq: 'weekly',
-  // TODO add lastmod with node file info from posts dir
-  autoLastmod: false
+  transform: async (config, path) => {
+    return {
+      loc: path,
+      changefreq: config.changefreq,
+      priority: pathLevelToPriority[getPathDepthLevel(path)],
+      lastmod: config.autoLastmod ? new Date().toISOString() : null,
+      alternateRefs: config.alternateRefs ?? []
+    };
+  }
 };
