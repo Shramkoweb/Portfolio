fd74ead7f9c60c536d2482b13d4c38960d1e3514
Serhii Shramko <s.shramko@macpaw.com>
2024-05-04 20:07:09 -0400

    feat: add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
+import { Analytics } from '@vercel/analytics/react';
 
 import { isProduction } from '@/lib/utils';
 
@@ -17,6 +18,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <Layout>
           {/* eslint-disable-next-line react/jsx-props-no-spreading */}
           <Component {...pageProps} />
+          <Analytics />
         </Layout>
       </ThemeProvider>
     </>
