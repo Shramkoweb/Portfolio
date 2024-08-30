ffb929f3bc24bd1c60c68acc5653731f13380d3e
Serhii Shramko <s.shramko@macpaw.com>
2024-08-30 17:54:59 -0400

    chore: rm unused const

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -1,7 +1,6 @@
 import * as Sentry from "@sentry/nextjs";
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
-const SENTRY_ENV = process.env.SENTRY_ENVIRONMENT || process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT;
 
 Sentry.init({
   dsn: SENTRY_DSN,
