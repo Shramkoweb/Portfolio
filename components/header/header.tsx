dbfe781457150193698d67079ce991531f5a4656
Serhii <s.shramko@macpaw.com>
2024-10-30 15:32:16 -0400

    chore: revert deletion

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import { HeaderLink } from '@/components/header-link';
 import { MobileMenu } from '@/components/mobile-menu/mobile-menu';
 import { ThemeChanger } from '@/components/theme-changer';
@@ -5,10 +7,7 @@ import { ThemeChanger } from '@/components/theme-changer';
 export function Header() {
   return (
     <header className="flex flex-col justify-center px-8" role="banner">
-      <nav
-        role="navigation"
-        className="relative mx-auto flex w-full max-w-3xl items-center justify-between border-gray-200 bg-gray-50 bg-opacity-60 pt-8 pb-8 text-gray-900 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 sm:pb-16"
-      >
+      <nav role="navigation" className="relative mx-auto flex w-full max-w-3xl items-center justify-between border-gray-200 bg-gray-50 bg-opacity-60 pt-8 pb-8 text-gray-900 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 sm:pb-16">
         <a href="#skip" className="skip-nav">
           Skip to main content
         </a>
@@ -20,8 +19,15 @@ export function Header() {
           <HeaderLink href="/dashboard" text="Dashboard" />
           <HeaderLink href="/snippets" text="Snippets" />
         </div>
-
         <div className="flex items-center justify-center">
+          <Link
+            href="/static/serhii-shramko-resume.pdf"
+            download
+            title="PDF document. Serhii Shramko - Resume. 30 KB"
+            className="ml-12 hidden rounded-lg border-2 border-gray-700 p-1 transition-all hover:bg-gray-200 sm:px-3 sm:py-2 md:inline-block dark:hover:bg-gray-800"
+          >
+            Resume
+          </Link>
           <ThemeChanger />
         </div>
       </nav>
