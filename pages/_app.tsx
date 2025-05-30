e41882a1f081ab3d8d88d8d2276811bca11b6b52
Serhii Shramko <shramko.web@yahoo.com>
2025-05-30 05:56:04 -0400

    style: fix import formatting for `SpeedInsights` in `_app.tsx`

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import { isProduction } from '@/lib/utils';
 
