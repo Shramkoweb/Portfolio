c3fbc515751fde31d0288fe379670764c2c0ae87
Serhii Shramko <shramko.web@yahoo.com>
2025-06-17 13:26:12 -0400

    refactor: improve a11y

--- components/categories/categories.tsx
+++ components/categories/categories.tsx
@@ -8,30 +8,34 @@ interface CategoriesProps {
 export function Categories(props: CategoriesProps) {
   const { categories } = props;
 
-  return (
-    <ul className="flex flex-wrap gap-2 text-gray-600 dark:text-gray-400">
-      <li key="all">
-        <Link
-          href="/blog"
-          className="inline-block rounded-lg border-2 border-gray-200 p-1 px-3 py-2 transition-all hover:bg-gray-200 dark:border-gray-700 dark:hover:bg-gray-800"
-        >
-          <span>All</span>
-        </Link>
-      </li>
-      {categories.map((item) => {
-        const formattedCategory = item.split('-').join(' ').trim();
+  const linkClasses = 'inline-block rounded-lg border-2 border-gray-200 p-1 px-3 py-2 transition-all hover:bg-gray-200 dark:border-gray-700 dark:hover:bg-gray-800';
 
-        return (
-          <li key={item}>
-            <Link
-              href={`/blog/category/${item.toLowerCase()}`}
-              className="inline-block rounded-lg border-2 border-gray-200 p-1 px-3 py-2 transition-all hover:bg-gray-200 dark:border-gray-700 dark:hover:bg-gray-800"
-            >
-              <span>{formattedCategory}</span>
-            </Link>
-          </li>
-        );
-      })}
-    </ul>
+  return (
+    <nav aria-label="Post categories">
+      <ul className="flex flex-wrap gap-2 text-gray-600 dark:text-gray-400">
+        <li key="all">
+          <Link
+            href="/blog"
+            className={linkClasses}
+          >
+            <span>All</span>
+          </Link>
+        </li>
+        {categories.map((item) => {
+          const displayCategory = item.split('-').join(' ').trim();
+
+          return (
+            <li key={item}>
+              <Link
+                href={`/blog/category/${item.toLowerCase()}`}
+                className={linkClasses}
+              >
+                {displayCategory}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
   );
 }
