f514db0becfbd034032d49ae98ebaa965770d2ec
shramko <serhii.shramko@betterme.world>
2025-11-19 12:06:15 -0500

    fix: correct typo in HTML attribute name from `data-relese` to `data-release`

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en" data-relese={process.env.APP_RELEASE_VERSION}>
+    <Html lang="en" data-release={process.env.APP_RELEASE_VERSION}>
       <Head>
         <link href="/static/favicons/favicon.ico" rel="shortcut icon" />
         <link href="/static/favicons/site.webmanifest" rel="manifest" />
