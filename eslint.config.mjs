a1475cac5a65c1f9b261a6c79d072af1d170bcf9
shramko <serhii.shramko@betterme.world>
2026-02-21 17:12:23 -0500

    chore: update eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,16 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  {
-    ignores: [
-      '.next/**',
-      'coverage/**',
-      'node_modules/**',
-      'public/**',
-      'out/**',
-      '**/*.d.ts'
-    ]
-  },
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
-];
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
+]);
 
 export default eslintConfig;
