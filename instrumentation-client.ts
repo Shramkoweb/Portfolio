35b43f7226c9e8bed28674b3adfa8862f5ca878b
shramko <serhii.shramko@betterme.world>
2026-04-17 19:58:52 -0400

    chore: small improvements

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -4,7 +4,7 @@ Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   environment: process.env.NODE_ENV,
   release: process.env.APP_RELEASE_VERSION,
-  tracesSampleRate: 0.1,
+  tracesSampleRate: 0,
   debug: false,
   sendDefaultPii: true,
   ignoreErrors: [
