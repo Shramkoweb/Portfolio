5445a2ae859eb39ed10cbcee0ad43e3f53ed96c4
shramko <serhii.shramko@betterme.world>
2026-05-04 14:46:46 -0400

    fix: match body background to page theme

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -58,7 +58,7 @@ export default function Document() {
           name="msapplication-config"
         />
       </Head>
-      <body className="bg-white dark:bg-black text-black dark:text-white">
+      <body className="bg-gray-50 dark:bg-gray-900 text-black dark:text-white">
         <Main />
         <NextScript />
       </body>
