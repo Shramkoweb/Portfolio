d6311fa5dcc3b6cdd7edaef588f75b80b988b31a
shramko <serhii.shramko@betterme.world>
2026-06-07 14:45:45 -0400

    refactor: move vercel script to dynamic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,21 @@
-import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
 import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
+import dynamic from 'next/dynamic';
 import { SWRConfig } from 'swr';
 
 import { Layout } from '@/components/layout';
 
 import '../styles/globals.css';
+
+const Analytics = dynamic(
+  () => import('@vercel/analytics/react').then((m) => m.Analytics),
+  { ssr: false },
+);
+
+const SpeedInsights = dynamic(
+  () => import('@vercel/speed-insights/next').then((m) => m.SpeedInsights),
+  { ssr: false },
+);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
